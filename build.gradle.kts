1bcbdbb6644c7e872c471a05cacc1982b4eb0c0a
hej090224 <fc49854985@gmail.com>
2026-09-06 16:54:26 +0900

    fix: add JUnit platform launcher for JUnit 6

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ subprojects {
     dependencies {
         "testImplementation"(platform(rootProject.libs.junit.bom))
         "testImplementation"(rootProject.libs.junit.jupiter)
+        "testRuntimeOnly"("org.junit.platform:junit-platform-launcher")
     }
 
     tasks.withType<Test>().configureEach {
